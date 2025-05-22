9a5be9edccdaf5e92aebaca2a6c260d760fa2296
NiklasMattsson <niklasmattson1@gmail.com>
2025-05-22 13:30:56 +0200

    async

--- ErrorHandling.Web/Controllers/HomeController.cs
+++ ErrorHandling.Web/Controllers/HomeController.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ErrorHandling.Web.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ErrorHandling.Web.Controllers;
 
 public class HomeController : Controller
 {
     [HttpGet("")]
-    public IActionResult Index()
+    public async Task <IActionResult> IndexAsync()
     {
+        var slowService = new SlowService();
+        await slowService.DoSlowWorkAsync();
         return View();
     }
 
     [HttpGet("/throw")]
     public IActionResult Throw()
     {
-        throw new Exception("test");
+        //throw new Exception("test");
+        return View();
     }
 }
